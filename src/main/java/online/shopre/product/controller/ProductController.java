5f9967a1097765987480e03968ddada48cbb385c
Kunal <4002kunal88@gmail.com>
2025-08-21 02:05:51 +0530

    get product by id and slug

--- src/main/java/online/shopre/product/controller/ProductController.java
+++ src/main/java/online/shopre/product/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -14,13 +15,29 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    // this needs to  be converted to be an admin API.
+    @PostMapping
+    public Product createProduct(@RequestBody Product product){
+        return productService.createProduct(product);
+    }
+
+
     @GetMapping
     public List<Product> getProducts(){
         return productService.getProducts();
     }
 
-    @PostMapping
-    public Product createProduct(@RequestBody Product product){
-        return productService.createProduct(product);
+    // get by slug.
+    @GetMapping("/slug/{slug}")
+    public Product getProductBySlug(@PathVariable String slug){
+        return productService.getProductBySlug(slug);
     }
+
+    // get by id.
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable UUID id){
+        return productService.getProduct(id);
+    }
+
+
 }
--- src/main/java/online/shopre/product/service/ProductService.java
+++ src/main/java/online/shopre/product/service/ProductService.java
@@ -4,10 +4,10 @@ import online.shopre.product.model.Product;
 import online.shopre.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.LocalDateTime;
+
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -16,12 +16,26 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    public List<Product> getProducts(){
-        return productRepository.findAll();
-    }
-
     public Product createProduct(Product product){
         return productRepository.save(product);
     }
 
+    public List<Product> getProducts(){
+        return productRepository.findAll();
+    }
+
+    public Product getProduct(UUID id){
+
+//        Optional<Product> product = productRepository.findById(id);
+//        if (product.isPresent()){
+//            return product.get();
+//        }
+//        return null
+
+        return productRepository.findById(id).orElse(null); //functional programming.
+    }
+
+    public Product getProductBySlug(String slug){
+        return productRepository.findBySlug(slug).orElse(null); //functional programming.
+    }
 }
